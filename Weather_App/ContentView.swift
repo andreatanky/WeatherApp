fc103a9a6b6d12998dabc1c4a6fe0e0d64060b3f
Andrea Tan <andreaxtan611@gmail.com>
2021-05-28 16:17:00 +0800

    Initial commit

---
+++ Weather_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather_App
+//
+//  Created by ANDREA TAN on 28/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
