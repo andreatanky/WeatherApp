4178453da78c4ef18cae9dfec85b793a082e72b4
Andrea Tan <andreaxtan611@gmail.com>
2021-05-28 22:00:28 +0800

    Add Network Manager

---
+++ Weather_App/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  Weather_App
+//
+//  Created by ANDREA TAN on 28/5/21.
+//
+
+import Foundation
+
+struct API {
+    static let key = "38e381b0cc27ff4d3525471afa8d1816"
+}
---
+++ Weather_App/Networking/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Weather_App
+//
+//  Created by ANDREA TAN on 28/5/21.
+//
+
+import Foundation
+
+extension API {
+    static let baseURLString = "https://api.openweathermap.org/data/2.5/"
+    
+    static func getURLFor(lat: Double, lon: Double) -> String {
+        return"\(baseURLString)oncall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial"
+    }
+}
