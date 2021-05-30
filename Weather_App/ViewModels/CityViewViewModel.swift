b39d367a589741a2433daa5f3ac94f0cd7de3409
Andrea Tan <andreaxtan611@gmail.com>
2021-05-30 11:58:05 +0800

    Add Lottie Animation and dependency

--- Weather_App/ViewModels/CityViewViewModel.swift
+++ Weather_App/ViewModels/CityViewViewModel.swift
@@ -151,14 +151,56 @@ final class CityViewViewModel: ObservableObject {
         case "13d":
             return "daySnow"
         case "13n":
-            return "nightThunderstorm"
-        case "14d":
-            return "nightThunderstorm"
-        case "14n":
-            return "nightThunderstorm"
+            return "nightSnow"
+        case "50d":
+            return "dayMist"
+        case "50n":
+            return "dayMist"
         default:
-            print("hello")
+            return "dayClearSky"
         }
-        return "hello"
+    }
+    
+    func getWeatherIconFor(icon: String) -> Image {
+        switch icon {
+        case "01d":
+            return Image(systemName: "sun.max.fill")
+        case "01n":
+            return Image(systemName: "moon.fill")
+        case "02d":
+            return Image(systemName: "cloud.sun.fill")
+        case "02n":
+            return Image(systemName: "cloud.moon.fill")
+        case "03d":
+            return Image(systemName: "cloud.fill")
+        case "03n":
+            return Image(systemName: "cloud.fill")
+        case "04d":
+            return Image(systemName: "cloud.fill")
+        case "04n":
+            return Image(systemName: "cloud.fill")
+        case "09d":
+            return Image(systemName: "cloud.drizzle.fill")
+        case "09n":
+            return Image(systemName: "cloud.drizzle.fill")
+        case "10d":
+            return Image(systemName: "cloud.heavyrain.fill")
+        case "10n":
+            return Image(systemName: "cloud.heavyrain.fill")
+        case "11d":
+            return Image(systemName: "cloud.bolt.fill")
+        case "11n":
+            return Image(systemName: "cloud.bolt.fill")
+        case "13d":
+            return Image(systemName: "cloud.snow.fill")
+        case "13n":
+            return Image(systemName: "cloud.snow.fill")
+        case "50d":
+            return Image(systemName: "cloud.fog.fill")
+        case "50n":
+            return Image(systemName: "cloud.fog.fill")
+        default:
+            return Image(systemName: "sun.max.fill")
+        }
     }
 }
---
+++ Weather_App/Views/LottieView.swift
@@ -0,0 +1,8 @@
+//
+//  LottieView.swift
+//  Weather_App
+//
+//  Created by ANDREA TAN on 30/5/21.
+//
+
+import Foundation
